4290b47dd19a61cfd59344e2fda346884518466f
bhimeshchauhan <bhimeshchauhan@gmail.com>
2018-09-15 13:28:43 -0500

    added script to accept the data from the giphy app

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+var myModule = require('./giphy_api/main.js');
+
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('this works');
+    res.write(myModule.testRoute());
     res.end();
 }).listen(3000);
 
 server.listen(port, hostname, () => {
-    console.log("The server is running at http://${hostname}:${port}/");
+    console.log("The server is running at http://",hostname,":",port);
 });
---
+++ giphy_api/main.js
@@ -0,0 +1,7 @@
+var mainModule = {
+    testRoute : function(){
+        return "this is hi from the main.js from giphy_api";
+    }
+};
+
+module.exports = mainModule;
