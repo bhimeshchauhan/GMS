fa0b722380781f67f15482d4128cc8d805a1d7dd
bhimeshchauhan <bhimeshchauhan@gmail.com>
2018-09-15 15:35:34 -0500

    made changes to giphy api to follow twilio

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-const http = require('http');
+// const http = require('http');
 const hostname = '127.0.0.1';
 // const port = 3000;
 const twilioRouter = require('./twillio_api/main');
-const myModule = require('./giphy_api/main.js');
 
 // Twilio Part
 app.use(express.json());
@@ -16,16 +15,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/twilio',twilioRouter);
 
 const port = process.env.PORT || 3000;
-const listener = app.listen(port, ()=> {
-    console.log('Your app is listening on port ' + listener.address().port);
-});
-//  Giphy part
+//
+// //  Giphy part
 // const server = http.createServer((req, res) => {
 //     res.writeHead(200, {'Content-Type': 'text/html'});
 //     console.log("the data is ", myModule.testRoute());
 //     res.write(myModule.testRoute());
 //     res.end();
 // }).listen(3000);
+
+
+const listener = app.listen(port, ()=> {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
 //
 // server.listen(port, hostname, () => {
 //     console.log("The server is running at http://",hostname,":",port);
--- twillio_api/main.js
+++ twillio_api/main.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const accountSid = 'ACd7e8899a64658c8e7bb9a4974fce1484';
 const authToken = '04184a719c52a0bccba4ba217034c714';
 const client = require('twilio')(accountSid, authToken);
@@ -10,7 +9,7 @@ router.post("/sms", (request, response)=> {
     console.log(request.body);
     let toUser = request.body.From;
     let text = request.body.Body;
-    let gifURL = getGIF.getUrl(text);
+    let gifURL = getGIF.giphy(text);
     client.messages
         .create({
             body: '',
