167abb23f12d940a10ec85c3a4b3fe7db109bbbe
bhimeshchauhan <bhimeshchauhan@gmail.com>
2018-09-15 15:13:21 -0500

    some bullshit node changes

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-
+const http = require('http');
+const hostname = '127.0.0.1';
+// const port = 3000;
 const twilioRouter = require('./twillio_api/main');
+const myModule = require('./giphy_api/main.js');
 
+// Twilio Part
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,13 +19,7 @@ const port = process.env.PORT || 3000;
 const listener = app.listen(port, ()=> {
     console.log('Your app is listening on port ' + listener.address().port);
 });
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const myModule = require('./giphy_api/main.js');
-
-
+//  Giphy part
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     console.log("the data is ", myModule.testRoute());
